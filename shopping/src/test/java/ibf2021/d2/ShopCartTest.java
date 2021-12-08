43e646261725fe2249f0b0b606ea43b73b0ebcc0
Chan Yew Yen <chan.yewyen@gmail.com>
2021-12-08 11:35:46 +0800

    Update tests for refactored implementation

--- shopping/src/test/java/ibf2021/d2/ShopCartTest.java
+++ shopping/src/test/java/ibf2021/d2/ShopCartTest.java
@@ -12,52 +12,55 @@ import org.junit.Test;
 
 public class ShopCartTest {
     private ShopCart sc;
+    private Cart cart;
 
     @Before
     public void setUp() throws Exception {
         sc = new ShopCart();
+        cart = sc.getCart();
     }
 
     @After
     public void tearDown() throws Exception {
         sc = null;
+        cart = null;
     }
 
     @Test
     public void addCartItems_OneItemAdded_CountShouldIncreaseByOne() {
-        int initialCount = sc.getItemsCount();
-        sc.add("apple");
-        int countAfterAddOne = sc.getItemsCount();
+        int initialCount = cart.getItemsCount();
+        cart.add("apple");
+        int countAfterAddOne = cart.getItemsCount();
         assertTrue(countAfterAddOne == initialCount + 1);
     }
 
     @Test
     public void deleteCartItems_OneItemDeleted_CountShouldDecreaseByOne() {
-        sc.add("apple, orange, pear");
-        int count = sc.getItemsCount();
-        sc.delete(0);
-        int countAfterDelete = sc.getItemsCount();
+        cart.add("apple, orange, pear");
+        int count = cart.getItemsCount();
+        cart.delete(0);
+        int countAfterDelete = cart.getItemsCount();
         assertTrue(countAfterDelete == count - 1);
     }
 
     @Test
     public void addCartItems_AddDuplicateItems_ShouldNotAddDuplicates() {
-        sc.add("apple, orange");
-        int count = sc.getItemsCount();
-        sc.add("apple");
-        sc.add("apple, apple");
-        int countAfterDuplicates = sc.getItemsCount();
+        cart.add("apple, orange");
+        int count = cart.getItemsCount();
+        cart.add("apple");
+        cart.add("apple, apple");
+        int countAfterDuplicates = cart.getItemsCount();
         assertEquals(countAfterDuplicates, count);
     }
 
     @Test
     public void deleteCartItems_RemoveItems_RemainingItemsShouldMatch () {
-        sc.add("apple, pie");
-        List<String> cartBeforeDelete = sc.getItems();
-        sc.add("orange, carrot");
-        assertTrue(sc.getItemsCount() == 4);
-        sc.delete(3);
-        sc.delete(2);
+        cart.add("apple, pie");
+        List<String> cartBeforeDelete = cart.getItems();
+        cart.add("orange, carrot");
+        assertTrue(cart.getItemsCount() == 4);
+        cart.delete(3);
+        cart.delete(2);
         List<String> expected = Arrays.asList("apple", "pie");
         assertEquals(expected, cartBeforeDelete);
     }
